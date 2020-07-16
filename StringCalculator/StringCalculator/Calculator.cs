ba7b6e3c07475d4bb209cee772ce3970652c6476
Riccardo Ferro <riccardo.ferro@itstalentfactory.onmicrosoft.com>
2020-07-16 10:31:57 +0200

    Risoluzione test somma di più numeri e \n

--- StringCalculator/StringCalculator/Calculator.cs
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringCalculator
 {
@@ -10,9 +11,28 @@ namespace StringCalculator
             {
                 return 0;
             }
-            if (str.Contains(","))
+            if (str.Contains(",") || str.Contains("\n"))
             {
-                string[] numbers = str.Split(",");
+                List<string> numbers = new List<string>();
+                string[] subStrings = str.Split(",");
+
+                foreach (string subString in subStrings)
+                {
+                    if (subString.Contains("\n"))
+                    {
+                        string[] bnSubStrings = subString.Split("\n");
+
+                        foreach (string bnSubString in bnSubStrings)
+                        {
+                            numbers.Add(bnSubString);
+                        }
+                    }
+                    else
+                    {
+                        numbers.Add(subString);
+                    }
+                }
+
                 int sum = 0;
                 foreach (string number in numbers)
                 {
